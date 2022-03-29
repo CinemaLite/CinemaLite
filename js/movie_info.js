33982c8c0cedc0116f7df5bee20616d577c21581
CinemaLite <102618738+CinemaLite@users.noreply.github.com>
2022-03-29 12:04:29 -0600

    Add files via upload

---
+++ js/movie_info.js
@@ -0,0 +1,28 @@
+var movie_info = { 'movies':[
+                            {
+                                'Name': 'The Batman',
+                                'Date': ['10 Abril 2022', '11 Abril 2022', '12 Abril 2022'],
+                                'Time': ['10:00-12:15','19:00-21:15', '21:30-23:45'],
+                            },
+                            
+                            {
+                                'Name': 'Uncharted',
+                                'Date': ['10 Abril 2022', '11 Abril 2022', '12 Abril 2022'],
+                                'Time':['12:00-13:45', '16:30-18:15', '23:00-00:45'],
+                            },
+                            
+                            {
+                                'Name':'Morbius',
+                                'Date': ['10 Abril 2022', '11 Abril 2022', '12 Abril 2022'],
+                                'Time':['14:00-16:15', '18:00-20:15', '21:30-23:45'],
+                            },
+                            
+]
+}
+
+var audi_list = { 'audis':[
+						{
+							'Rows':'6',
+							'Columns':'6'
+						},
+]}
