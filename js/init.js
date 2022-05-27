06831418e8adeeee4a7686ee9401970749f33bc6
CinemaLite <102618738+CinemaLite@users.noreply.github.com>
2022-05-27 13:07:33 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,16 +1,6 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
-
-import firebase from "firebase/app";
-import "firebase/auth";
-
-// Docs: https://source.corp.google.com/piper///depot/google3/third_party/devsite/firebase/en/docs/auth/web/google-signin.md
-
-function googleProvider() {
-  // [START auth_google_provider_create]
-  var provider = new firebase.auth.GoogleAuthProvider();
-
 firebase.initializeApp({
   apiKey: "AIzaSyAbqZ98ysatV3ozNJCG_LniUNYwvA8o-Q0",
   authDomain: "cinemalite-92071.firebaseapp.com",
