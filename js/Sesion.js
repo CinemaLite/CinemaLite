35ea9d20a71a93ee2d911dc7a31b9ce9e03e79bb
CinemaLite <102618738+CinemaLite@users.noreply.github.com>
2022-05-27 13:16:21 -0500

    Update Sesion.js

--- js/Sesion.js
+++ js/Sesion.js
@@ -1,3 +1,6 @@
+// These samples are intended for Web so this import would normally be
+// done in HTML however using modules here is more convenient for
+// ensuring sample correctness offline.
 import firebase from "firebase/app";
 import "firebase/auth";
 
@@ -12,19 +15,6 @@ function makeGoogleCredential(googleUser) {
   // [END auth_make_google_credential]
 }
 
-function makeFacebookCredential(response) {
-  // [START auth_make_facebook_credential]
-  var credential = firebase.auth.FacebookAuthProvider.credential(
-    response.authResponse.accessToken);
-  // [END auth_make_facebook_credential]
-}
-
-function makeEmailCredential(email, password) {
-  // [START auth_make_email_credential]
-  var credential = firebase.auth.EmailAuthProvider.credential(email, password);
-  // [END auth_make_email_credential]
-}
-
 function signOut() {
   // [START auth_sign_out]
   firebase.auth().signOut().then(() => {
@@ -35,21 +25,6 @@ function signOut() {
   // [END auth_sign_out]
 }
 
-function authStateListener() {
-  // [START auth_state_listener]
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      var uid = user.uid;
-      // ...
-    } else {
-      // User is signed out
-      // ...
-    }
-  });
-  // [END auth_state_listener]
-}
 
 function currentUser() {
   // [START auth_current_user]
@@ -65,33 +40,7 @@ function currentUser() {
   // [END auth_current_user]
 }
 
-function setLanguageCode() {
-  // [START auth_set_language_code]
-  firebase.auth().languageCode = 'it';
-  // To apply the default browser preference instead of explicitly setting it.
-  // firebase.auth().useDeviceLanguage();
-  // [END auth_set_language_code]
-}
 
-function authWithCredential(credential) {
-  // [START auth_signin_credential]
-  // Sign in with the credential from the user.
-  firebase.auth()
-    .signInWithCredential(credential)
-    .then((result) => {
-      // Signed in 
-      // ...
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // ...
-    });
-  // [END auth_signin_credential]
-}
 
 function signInRedirect(provider) {
   // [START auth_signin_redirect]
@@ -99,13 +48,4 @@ function signInRedirect(provider) {
   // [END auth_signin_redirect]
 }
 
-function initializeWithCustomDomain() {
-  // [START auth_init_custom_domain]
-  firebase.initializeApp({
-    apiKey: '...',
-    // By default, authDomain is '[YOUR_APP].firebaseapp.com'.
-    // You may replace it with a custom domain.
-    authDomain: '[YOUR_CUSTOM_DOMAIN]'
-  });
-  // [END auth_init_custom_domain]
 }
