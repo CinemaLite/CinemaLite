b6cac970f389950661131c2ba491e81725b4c0b6
CinemaLite <102618738+CinemaLite@users.noreply.github.com>
2022-05-29 18:21:51 -0500

    Update Sesion.js

--- js/Sesion.js
+++ js/Sesion.js
@@ -1,53 +1,14 @@
-// These samples are intended for Web so this import would normally be
-// done in HTML however using modules here is more convenient for
-// ensuring sample correctness offline.
-
-// ==========================================================================================
-// Docs: Snippets in this file are "general purpose" and are used on more than one docs page
-// ==========================================================================================
-firebase.initializeApp(firebaseConfig);
-
-	const auth=firebase.auth();
-
-
-function makeGoogleCredential(googleUser) {
-  // [START auth_make_google_credential]
-  var credential = firebase.auth.GoogleAuthProvider.credential(
-    googleUser.getAuthResponse().id_token);
-  // [END auth_make_google_credential]
-}
-
-function signOut() {
-  // [START auth_sign_out]
-  firebase.auth().signOut().then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
-  // [END auth_sign_out]
-}
-
-
-function currentUser() {
-  // [START auth_current_user]
-  const user = firebase.auth().currentUser;
-
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    // ...
-  } else {
-    // No user is signed in.
+// Using a redirect.
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token.
+    var token = result.credential.accessToken;
   }
-  // [END auth_current_user]
-}
+  var user = result.user;
+});
 
-
-
-function signInRedirect(provider) {
-  // [START auth_signin_redirect]
-  firebase.auth().signInWithRedirect(provider);
-  // [END auth_signin_redirect]
-}
-
-}
+// Start a sign in process for an unauthenticated user.
+var provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('profile');
+provider.addScope('email');
+firebase.auth().signInWithRedirect(provider);
