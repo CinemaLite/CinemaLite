f26abc0b3faa9c09ff226c77650a28eb4b36c16e
CinemaLite <102618738+CinemaLite@users.noreply.github.com>
2022-05-29 19:04:19 -0500

    Update movie_info.js

--- js/movie_info.js
+++ js/movie_info.js
@@ -1,19 +1,19 @@
 var movie_info = { 'movies':[
                             {
                                 'Name': 'The Batman',
-                                'Date': ['10 Abril 2022', '11 Abril 2022', '12 Abril 2022'],
+                                'Date': ['27 Mayo 2022', '28 Mayo 2022', '29 Mayo 2022'],
                                 'Time': ['10:00-12:15','19:00-21:15', '21:30-23:45'],
                             },
                             
                             {
-                                'Name': 'Uncharted',
-                                'Date': ['10 Abril 2022', '11 Abril 2022', '12 Abril 2022'],
+                                'Name': 'Sonic 2',
+                                'Date': ['27 Mayo 2022', '28 Mayo 2022', '29 Mayo 2022'],
                                 'Time':['12:00-13:45', '16:30-18:15', '23:00-00:45'],
                             },
                             
                             {
-                                'Name':'Morbius',
-                                'Date': ['10 Abril 2022', '11 Abril 2022', '12 Abril 2022'],
+                                'Name':'Dr Strange',
+                                'Date': ['27 Mayo 2022', '28 Mayo 2022', '29 Mayo 2022'],
                                 'Time':['14:00-16:15', '18:00-20:15', '21:30-23:45'],
                             },
                             
