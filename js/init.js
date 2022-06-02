03822cd821650eb6ca7acc09beb4961187936023
CinemaLite <102618738+CinemaLite@users.noreply.github.com>
2022-06-01 21:07:36 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAbqZ98ysatV3ozNJCG_LniUNYwvA8o-Q0",
+  authDomain: "cinemalite-92071.firebaseapp.com",
+  projectId: "cinemalite-92071",
+  storageBucket: "cinemalite-92071.appspot.com",
+  messagingSenderId: "330227688776",
+  appId: "1:330227688776:web:787cc6915d74a5f524bf7d",
+  measurementId: "G-HW1B5HCVZH"
+});
