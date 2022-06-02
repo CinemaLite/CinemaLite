8e4e84b8a8fbd7f47ca2031b193b3087bad637d6
CinemaLite <102618738+CinemaLite@users.noreply.github.com>
2022-06-01 20:06:58 -0500

    Update now_showing.js

--- js/now_showing.js
+++ js/now_showing.js
@@ -1,19 +1,19 @@
 var movie_info = { 'movies':[
                             {
                                 'Name': 'THE BATMAN',
-                                'Date': ['8 Junio', '9 Junio', '10 Junio','11 Junio', '12 Junio', '13 Junio','14 Junio'],
+                                'Date': ['8 Junio', '9 Junio', '10 Junio'],
                                 'Time': ['10:00 - 12:15','19:00 - 21:15', '21:30 - 23:45'],
                             },
                             
                             {
                                 'Name': 'DR STRANGE MULTIVERSE OF MADNESS',
-                                'Date': ['8 Junio', '9 Junio', '10 Junio','11 Junio', '12 Junio', '13 Junio','14 Junio'],
+                                'Date': ['8 Junio', '9 Junio', '10 Junio'],
                                 'Time':['12:00 - 13:45', '16:30 - 18:15', '23:00 - 00:45'],
                             },
                             
                             {
                                 'Name':'SONIC 2',
-                                'Date': ['8 Junio', '9 Junio', '10 Junio','11 Junio', '12 Junio', '13 Junio','14 Junio'],
+                                'Date': ['8 Junio', '9 Junio', '10 Junio'],
                                 'Time':['14:00 - 16:15', '18:00 - 20:15', '21:30 - 23:45'],
                             },
 	 						
