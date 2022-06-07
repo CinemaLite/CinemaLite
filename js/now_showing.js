ae62f04fbfa07d74c4d5fe38b42531fbdc1aaaf6
CinemaLite <102618738+CinemaLite@users.noreply.github.com>
2022-06-07 12:51:55 -0500

    Update now_showing.js

--- js/now_showing.js
+++ js/now_showing.js
@@ -1,19 +1,19 @@
 var movie_info = { 'movies':[
                             {
                                 'Name': 'The Batman',
-                                'Date': ['06 Junio 2022', '07 Junio 2022', '08 Junio 2022'],
+                                'Date': ['07 Junio 2022', '08 Junio 2022', '09 Junio 2022'],
                                 'Time': ['10:00 - 12:15','19:00 - 21:15', '21:30 - 23:45'],
                             },
                             
                             {
                                 'Name': 'Dr Strange 2',
-                                'Date': ['06 Junio 2022', '07 Junio 2022', '08 Junio 2022'],
+                                'Date': ['07 Junio 2022', '08 Junio 2022', '09 Junio 2022'],
                                 'Time':['12:00 - 13:45', '16:30 - 18:15', '23:00 - 00:45'],
                             },
                             
                             {
                                 'Name':'Sonic 2',
-                                'Date': ['06 Junio 2022', '07 Junio 2022', '08 Junio 2022'],
+                                'Date': ['07 Junio 2022', '08 Junio 2022', '09 Junio 2022'],
                                 'Time':['14:00 - 16:15', '18:00 - 20:15', '21:30 - 23:45'],
                             },
                             
