9fccf05b93c642e4d73775fbcb80107d521a53c8
CinemaLite <102618738+CinemaLite@users.noreply.github.com>
2022-06-22 08:21:19 -0500

    Update now_showing.js

--- js/now_showing.js
+++ js/now_showing.js
@@ -1,34 +1,34 @@
 var movie_info = { 'movies':[
                             {
                                 'Name': 'Lightyear',
-                                'Date': ['21 Junio 2022', '22 Junio 2022', '23 Junio 2022'],
+                                'Date': ['22 Junio 2022', '23 Junio 2022', '24 Junio 2022'],
                                 'Time': ['10:00 - 12:15','19:00 - 21:15', '21:30 - 23:45'],
                             },
                             
                             {
                                 'Name': 'Sonic 2',
-                                'Date': ['21 Junio 2022', '22 Junio 2022', '23 Junio 2022'],
+                                'Date': ['22 Junio 2022', '23 Junio 2022', '24 Junio 2022'],
                                 'Time':['12:00 - 13:45', '16:30 - 18:15', '23:00 - 00:45'],
                             },
                             
                             {
                                 'Name':'Dr Strange Multiverse of Madness',
-                                'Date': ['21 Junio 2022', '22 Junio 2022', '23 Junio 2022'],
+                                'Date': ['22 Junio 2022', '23 Junio 2022', '24 Junio 2022'],
                                 'Time':['14:00 - 16:15', '18:00 - 20:15', '21:30 - 23:45'],
                             },
 			    {
                                 'Name':'Top Gun Maverick',
-                                'Date': ['21 Junio 2022', '22 Junio 2022', '23 Junio 2022'],
+                                'Date': ['22 Junio 2022', '23 Junio 2022', '24 Junio 2022'],
                                 'Time': ['10:00 - 12:15','19:00 - 21:15', '21:30 - 23:45'],
                             },
 							{
                                 'Name':'Morbius',
-                                'Date': ['21 Junio 2022', '22 Junio 2022', '23 Junio 2022'],
+                                'Date': ['22 Junio 2022', '23 Junio 2022', '24 Junio 2022'],
                                 'Time':['12:00 - 13:45', '16:30 - 18:15', '23:00 - 00:45'],
                             },
 							{
                                 'Name':'Jurassic World Dominion',
-                                'Date': ['21 Junio 2022', '22 Junio 2022', '23 Junio 2022'],
+                                'Date': ['22 Junio 2022', '23 Junio 2022', '24 Junio 2022'],
                                 'Time':['14:00 - 16:15', '18:00 - 20:15', '21:30 - 23:45'],
                             },
                             
